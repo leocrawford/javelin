8cfaa1dbdaf0ee73fb43e681b05fbadb8a864543
leo <leo@Leo-Desktop>
2013-05-09 21:18:24 +0100

    cleared all code, to start over

--- src/main/java/com/crypticbit/javelin/neo4j/strategies/Identity.java
+++
@@ -1,7 +0,0 @@
-package com.crypticbit.javelin.neo4j.strategies;
-
-import java.io.Serializable;
-
-public interface Identity extends Serializable, Comparable<Identity> {
-
-}
--- src/main/java/com/crypticbit/javelin/neo4j/types/RelationshipTypes.java
+++
@@ -1,7 +0,0 @@
-package com.crypticbit.javelin.neo4j.types;
-
-import org.neo4j.graphdb.RelationshipType;
-
-public enum RelationshipTypes implements RelationshipType {
-    ARRAY, MAP, INCOMING_VERSION, VERSION, ROOT
-}
